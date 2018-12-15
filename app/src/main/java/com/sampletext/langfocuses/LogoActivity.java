e8cc460ba6c6c9f6debcb7cd037d984586763148
sampletext32 <sampletext32@bk.ru>
2018-12-15 17:40:23 +0300

    Initial Commit

---
+++ app/src/main/java/com/sampletext/langfocuses/LogoActivity.java
@@ -0,0 +1,68 @@
+package com.sampletext.langfocuses;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.Interpolator;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+
+public class LogoActivity extends Activity {
+
+    private ImageView logoImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+        logoImageView = findViewById(R.id.logoImageView);
+
+        Static.SetPortrait(this);
+        setContentView(R.layout.activity_logo);
+        logoImageView = findViewById(R.id.logoImageView);
+
+        float animScaleStart = 1f;
+        float animScaleEnd   = 1f;
+        float animRelativeTo = 0.5f;
+
+        Animation anim = new ScaleAnimation(
+                animScaleStart, animScaleEnd,
+                animScaleStart, animScaleEnd,
+                Animation.RELATIVE_TO_SELF, animRelativeTo,
+                Animation.RELATIVE_TO_SELF, animRelativeTo);
+        anim.setFillAfter(true); //оставить результат, не сбрасывать на начало
+        anim.setDuration(2000);
+        anim.setInterpolator(new Interpolator() {
+
+            @Override
+            public float getInterpolation(float v) {
+                return (float) Math.tan(v);//интерполяция по тангенсу
+            }
+        });
+        anim.setAnimationListener(animationListener);
+        logoImageView.setAnimation(anim);
+
+    }
+
+    Animation.AnimationListener animationListener = new Animation.AnimationListener() {
+
+        Intent intent;
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+            intent = new Intent(getApplicationContext(), Logo2Activity.class);
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            startActivity(intent);
+            finish();
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+    };
+}
---
+++ app/src/main/java/com/sampletext/langfocuses/Static.java
@@ -0,0 +1,18 @@
+package com.sampletext.langfocuses;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+
+import java.util.Calendar;
+import java.util.Random;
+
+public class Static {
+    public static Random rnd = new Random(Calendar.getInstance().get(Calendar.MILLISECOND));
+
+    public static float  DiagonalInches = 0f;
+    public static void SetPortrait(Activity a) {
+        if (a != null) {
+            a.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+    }
+}
