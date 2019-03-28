a1ba2cf702ade019790e0485b19d26bed0483259
sampletext32 <sampletext32@bk.ru>
2019-03-28 19:12:45 +0300

    banner work finished

--- app/src/main/java/com/sampletext/langfocuses/ScheduleActivity.java
+++ app/src/main/java/com/sampletext/langfocuses/ScheduleActivity.java
@@ -1,11 +1,14 @@
 package com.sampletext.langfocuses;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +17,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -67,11 +71,12 @@ public class ScheduleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
         final TextView schedule_content = findViewById(R.id.schedule_content);
-        //final ProgressBar schedule_progressbar = findViewById(R.id.schedule_progressbar);
+        final ProgressBar schedule_progressbar = findViewById(R.id.schedule_progressbar);
         final ImageView schedule_imageview = findViewById(R.id.schedule_image);
         final LinearLayout schedule_container = findViewById(R.id.schedule_container);
+        final Button schedule_btn_more = findViewById(R.id.schedule_btn_more);
 
-        schedule_imageview.setOnClickListener(new View.OnClickListener() {
+        schedule_btn_more.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!imgTargetUrl.equals("")) {
@@ -83,6 +88,27 @@ public class ScheduleActivity extends AppCompatActivity {
             }
         });
 
+        schedule_btn_more.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_HOVER_ENTER:
+                    case MotionEvent.ACTION_DOWN:
+                        v.getBackground().setColorFilter(Color.parseColor("#AAAAAA"), PorterDuff.Mode.SRC_ATOP);
+                        break;
+                    case MotionEvent.ACTION_HOVER_EXIT:
+                    case MotionEvent.ACTION_UP:
+                        v.getBackground().clearColorFilter();
+                        break;
+                    case MotionEvent.ACTION_CANCEL:
+                        v.getBackground().clearColorFilter();
+                        break;
+                    default:
+                }
+                return false;
+            }
+        });
+
         Button btnBack = findViewById(R.id.btn_back);
         btnBack.getBackground().setColorFilter(Color.parseColor("#87704A"), PorterDuff.Mode.SRC_ATOP);
         btnBack.setOnTouchListener(btnBackOnTouchListener);
@@ -91,51 +117,62 @@ public class ScheduleActivity extends AppCompatActivity {
         //final String bannerUrl = "http://itpark32.ru/banner/";
         final String bannerUrl = "http://institutnlp.tilda.ws/banerfy";
 
+        ConnectivityManager cm =
+                (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Document document = Jsoup.connect(bannerUrl).get();
-                    Element a = document.getElementsByTag("a").get(0);
-                    final String ahref = a.attr("abs:href");
-                    Element img = a.child(0);
-                    final String imgsrc = img.attr("abs:src");
-                    URL u = new URL(imgsrc);
-                    InputStream inputStream = u.openStream();
-                    Bitmap bitmapp = BitmapFactory.decodeStream(inputStream);
-                    final Bitmap bitmap  = Bitmap.createScaledBitmap(bitmapp, 620, 620, false);
-                    inputStream.close();
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            imgTargetUrl = ahref;
-                            schedule_imageview.setImageBitmap(bitmap);
-                            schedule_imageview.invalidate();
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
 
-                            //Toast.makeText(getApplicationContext(), imgsrc + " to " + ahref, Toast.LENGTH_LONG).show();
-                        }
-                    });
+        boolean isConnected = (activeNetwork != null) && activeNetwork.isConnected();
 
-                } catch (final Exception e) {
-                    e.printStackTrace();
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(getApplicationContext(), "Loading Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
-                        }
-                    });
-                } finally {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            //schedule_container.removeView(schedule_progressbar);
-                        }
-                    });
+        if (isConnected) {
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Document document = Jsoup.connect(bannerUrl).timeout(5000).get();
+                        Element a = document.getElementsByTag("a").get(0);
+                        final String ahref = a.attr("abs:href");
+                        Element img = a.child(0);
+                        final String imgsrc = img.attr("abs:data-original");
+                        URL u = new URL(imgsrc);
+                        InputStream inputStream = u.openStream();
+                        final Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                        inputStream.close();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                imgTargetUrl = ahref;
+                                schedule_imageview.setImageBitmap(bitmap);
+
+                                schedule_imageview.invalidate();
+                            }
+                        });
+
+                    } catch (final Exception e) {
+                        e.printStackTrace();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                schedule_container.removeView(schedule_imageview);
+                                schedule_content.setText("Не удалось установить соединение с интернетом");
+                                schedule_container.removeView(schedule_btn_more);
+                            }
+                        });
+                    }
                 }
-            }
-        }).start();
+            }).start();
+        } else {
+            schedule_container.removeView(schedule_imageview);
+            schedule_content.setText("Не удалось установить соединение с интернетом");
+            schedule_btn_more.setOnClickListener(null);
+            schedule_btn_more.setOnTouchListener(null);
 
+
+            schedule_btn_more.getBackground().setColorFilter(Color.parseColor("#AAAAAA"), PorterDuff.Mode.SRC_ATOP);;
+            //schedule_container.removeView(schedule_btn_more);
+        }
+        schedule_container.removeView(schedule_progressbar);
 
     }
 }
