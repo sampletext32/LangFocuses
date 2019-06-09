f65cf594634f600a9dbbf5937ecc9c98d8e74bcc
sampletext32 <sampletext32@bk.ru>
2019-06-09 15:12:41 +0300

    Banner Moved To Main Activity Fixed ProgressBar Not Showing Disabled HardwareAcceleration To Prevent Large Bitmaps Creation

--- app/src/main/java/com/sampletext/langfocuses/MainActivity.java
+++ app/src/main/java/com/sampletext/langfocuses/MainActivity.java
@@ -2,22 +2,38 @@ package com.sampletext.langfocuses;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.Typeface;
-import android.os.Build;
+import android.graphics.drawable.ColorDrawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.content.res.ResourcesCompat;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.crashlytics.android.Crashlytics;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.InputStream;
+import java.net.URL;
 
 public class MainActivity extends Activity {
+
+    String imgTargetUrl = "";
 
     View.OnTouchListener btn_Highlight_OnTouchListener = new View.OnTouchListener() {
 
@@ -65,13 +81,6 @@ public class MainActivity extends Activity {
             startActivity(intent);
         }
     };
-    private View.OnClickListener BtnSchedule_OnClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(getApplicationContext(), ScheduleActivity.class);
-            startActivity(intent);
-        }
-    };
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -102,24 +111,103 @@ public class MainActivity extends Activity {
         Button _btnChooseDeck = findViewById(R.id.btn_choose_deck);
         Button _btnAbout = findViewById(R.id.btn_about);
         Button _btnHowToUse = findViewById(R.id.btn_how_to_use);
-        Button _btnSchedule = findViewById(R.id.btn_schedule);
 
         Static.fitText(header_antifocusy);
         Static.fitText(header_yazyka);
         Static.fitText(_btnAbout);
         Static.fitText(_btnChooseDeck);
         Static.fitText(_btnHowToUse);
-        Static.fitText(_btnSchedule);
 
         _btnHowToUse.setOnTouchListener(btn_Highlight_OnTouchListener);
         _btnChooseDeck.setOnTouchListener(btn_Highlight_OnTouchListener);
         _btnAbout.setOnTouchListener(btn_Highlight_OnTouchListener);
-        _btnSchedule.setOnTouchListener(btn_Highlight_OnTouchListener);
 
         _btnHowToUse.setOnClickListener(BtnHowToUse_OnClickListener);
         _btnChooseDeck.setOnClickListener(BtnChooseDeck_OnClickListener);
         _btnAbout.setOnClickListener(BtnAbout_OnClickListener);
-        _btnSchedule.setOnClickListener(BtnSchedule_OnClickListener);
+
+
+        //BANNER LOADING ROUTINE
+
+        final TextView schedule_content = findViewById(R.id.schedule_content);
+        final ProgressBar schedule_progressbar = findViewById(R.id.schedule_progressbar);
+        final ImageView schedule_imageview = findViewById(R.id.schedule_image);
+        final FrameLayout schedule_container = findViewById(R.id.schedule_container);
+
+        schedule_imageview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!imgTargetUrl.equals("")) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(imgTargetUrl));
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Image Is Not Loaded", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        final String bannerUrl = "http://institutnlp.tilda.ws/banerfy";
+
+        ConnectivityManager cm =
+                (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        boolean isConnected = (activeNetwork != null) && activeNetwork.isConnected();
+
+        if (isConnected) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Document document = Jsoup.connect(bannerUrl).timeout(5000).get();
+                        Element a = document.getElementsByTag("a").get(0);
+                        final String ahref = a.attr("abs:href");
+                        Element img = a.child(0);
+                        final String imgsrc = img.attr("abs:data-original");
+                        URL u = new URL(imgsrc);
+                        InputStream inputStream = u.openStream();
+                        final Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                        inputStream.close();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                imgTargetUrl = ahref;
+                                schedule_imageview.setImageBitmap(bitmap);
+                                schedule_imageview.invalidate();
+                            }
+                        });
+
+                    }
+                    catch (final Exception e) {
+                        e.printStackTrace();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                schedule_container.removeView(schedule_imageview);
+                                schedule_content.setText("Не удалось установить соединение с интернетом");
+                            }
+                        });
+                    }
+                    finally {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                schedule_container.removeView(schedule_progressbar);
+                            }
+                        });
+                    }
+                }
+            }).start();
+        } else {
+            //schedule_container.removeView(schedule_imageview);
+            ColorDrawable colorDrawable = new ColorDrawable();
+            colorDrawable.setColor(Color.parseColor("#55E6C287"));
+            schedule_imageview.setBackground(colorDrawable);
+            schedule_content.setText("Не удалось установить соединение с интернетом");
+            //schedule_container.removeView(schedule_btn_more);
+        }
+        schedule_container.removeView(schedule_progressbar);
     }
 }
 
